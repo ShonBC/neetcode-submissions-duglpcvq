70f324c15685a5e0602d6056f4a42f330c6fd158
ShonBC <79477080+ShonBC@users.noreply.github.com>
2026-03-29 15:19:15 -0700

    Add: rotting-fruit - submission-11

---
+++ Data Structures & Algorithms/rotting-fruit/submission-11.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        // Time and Space Complexity: O(rows * cols)
+        set<pair<int, int>> visited;
+        set<pair<int, int>> moves{{0,1},{1,0},{0,-1},{-1,0}};
+        deque<pair<int, int>> q;
+        int fresh{0}, time{0};
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                int node{grid[i][j]};
+                if (node == 1) fresh++;
+                else if (node == 2) {
+                    q.push_back({i,j});
+                    visited.insert({i,j});
+                }
+            }
+        }
+        while (!q.empty() && fresh) {
+            int layer{q.size()};
+            for (int i = 0; i < layer; i++) {
+                auto [r, c] = q.front();
+                q.pop_front();
+                cout << r << " " << c << " " << fresh << " " << time << '\n';
+                for (auto [dr, dc] : moves) {
+                    int nr{r + dr}, nc{c + dc};
+                    if (min(nr, nc) >= 0 && nr < grid.size() && nc < grid[nr].size() && !visited.contains({nr, nc})) {
+                        visited.insert({nr, nc});
+                        if (grid[nr][nc] == 1) {
+                            q.push_back({nr, nc});
+                            fresh--;
+                        }
+                    }
+                }
+            }
+            time++;
+        }
+        return !fresh ? time : -1;
+    }
+};
