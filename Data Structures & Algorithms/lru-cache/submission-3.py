1a9d3c60a99c32f108e00b09b956f7738652e61f
ShonBC <79477080+ShonBC@users.noreply.github.com>
2026-04-29 23:28:26 -0700

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,19 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        ans = self.cache.get(key, -1)
+        if ans != -1:
+            self.cache.move_to_end(key)
+        return ans
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
